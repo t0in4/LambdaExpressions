475bb9dc02800a67a79467e7f500ae9463f849e1
t0in4 <t0in4@yahoo.com>
2023-02-10 20:49:38 +0900

    20:49

---
+++ src/control2/Main.java
@@ -0,0 +1,8 @@
+package control2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Operator.unaryOperator.applyAsLong(5));
+    }
+}
---
+++ src/control2/Operator.java
@@ -0,0 +1,7 @@
+package control2;
+
+import java.util.function.LongUnaryOperator;
+
+class Operator {
+    public static LongUnaryOperator unaryOperator = x -> (x + 1) % 2 == 0 ? x + 1 : x + 2;
+}
